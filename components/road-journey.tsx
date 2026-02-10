c6d56c6974e9af33c8dd15005470ed5d09f54ce7
Colin James <colinrichjames@gmail.com>
2026-02-09 22:15:31 -0800

    Update road-journey.tsx

--- components/road-journey.tsx
+++ components/road-journey.tsx
@@ -156,7 +156,7 @@ export function RoadJourney() {
             style={{ top: carTop, x: "-50%", y: "-50%" }}
             aria-hidden="true"
           >
-            <CarIcon className="w-10 md:w-14 text-primary rotate-90" />
+            <CarIcon className="w-10 md:w-14 text-primary" />
           </motion.div>
 
           {/* Stop cards */}
